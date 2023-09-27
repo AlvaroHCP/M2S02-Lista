6f43af1f3a9f4d6a370d584d4dd6a6b57d0cb6b9
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2023-09-26 22:09:47 -0300

    Finished.

--- src/exercicio5/FrequenciaCardiaca.java
+++ src/exercicio5/FrequenciaCardiaca.java
@@ -11,29 +11,42 @@ public class FrequenciaCardiaca {
         this.nome = nome;
         this.sobrenome = sobrenome;
         this.dataDeNascimento = dataNascimento;
+
+        idadeEmAnos(dataNascimento);
+        frequenciaCardiacaMaxima();
+        frequenciaCardiacaAlvo();
     }
 
     @Override
     public String toString() {
-        return "FrequenciaCardiaca{" +
-                "nome='" + nome + '\'' +
-                ", sobrenome='" + sobrenome + '\'' +
-                ", dataDeNascimento='" + dataDeNascimento + '\'' +
-                ", idade=" + idade +
-                ", frequenciaCardiacaMaxima=" + frequenciaCardiacaMaxima +
-                ", frequenciaCardiacaAlvo50=" + frequenciaCardiacaAlvo50 +
-                ", frequenciaCardiacaAlvo85=" + frequenciaCardiacaAlvo85 +
-                '}';
+        return  "Nome= " + nome + ' ' + sobrenome + "\n" +
+                "Data de nascimento= " + dataDeNascimento + "\n" +
+                "Idade= " + idade + " anos" + "\n" +
+                "Frequencia cardiaca maxima= " + frequenciaCardiacaMaxima + " bpm \n" +
+                "Frequencia cardiaca alvo= (" + frequenciaCardiacaAlvo50 +
+                " - " + frequenciaCardiacaAlvo85 + ") bpm."
+                ;
     }
 
     public int idadeEmAnos(String dataNascimento) {
         String[] idade = dataNascimento.split("/");
-        System.out.println(idade[2]);
+//        System.out.println(idade[2]);
         this.idade = 2023 - Integer.parseInt(idade[2]);
         return this.idade;
     }
 
+    public int frequenciaCardiacaMaxima(){
+        this.frequenciaCardiacaMaxima = 220 - this.idade;
 
+        return this.frequenciaCardiacaMaxima;
+}
+
+    public String frequenciaCardiacaAlvo(){
+        this.frequenciaCardiacaAlvo50 = (int) (0.5 * this.frequenciaCardiacaMaxima);
+        this.frequenciaCardiacaAlvo85 = (int) (0.85 * this.frequenciaCardiacaMaxima);
+        return "Frequencia Cardíaca Alvo fica entre " + this.frequenciaCardiacaAlvo50 + " e " +
+                this.frequenciaCardiacaAlvo85 + ".";
+    }
 
     public String getNome() {
         return nome;
